5360177fb24d00e8a60f0968018fb30f6d2a674e
Sebastian Bušek <sebastian.busek@konicaminolta.cz>
2016-04-07 08:22:13 +0200

    Fixed unit tests and added in memory repository

--- Idea7.Repository.EntityFramework.Tests/RepositoryTests.cs
+++ Idea7.Repository.EntityFramework.Tests/RepositoryTests.cs
@@ -1,42 +1,29 @@
-﻿using System;
+﻿using Idea7.Repository.EntityFramework.Tests.Mocks;
 
-using Idea7.Repository.EntityFramework.Tests.Mocks;
-using Idea7.UnitOfWork;
-using Idea7.UnitOfWork.EntityFramework;
-
-using Microsoft.Data.Entity;
 using Xunit;
 
 namespace Idea7.Repository.EntityFramework.Tests
 {
-    public class RepositoryTests
+    public class RepositoryTests : IClassFixture<RepositoryFixture>
     {
-        private const string SupermanKey = "a1943a62-34e7-4cec-8617-192a431ef689";
-        private const string ZodKey = "44b20652-2f1b-44cb-961d-c2e254d240a6";
-        private const string BatmanKey = "8c1f0e85-a665-4d2c-bfb3-0258c9b16b55";
-        private const string IvyKey = "5b41516a-f2cb-4256-88a7-3b91535639d1";
-        private const string HarleyKey = "1635bbb8-4168-4b42-a717-5b7813d913d7";
+        public const string SupermanKey = "a1943a62-34e7-4cec-8617-192a431ef689";
+        public const string ZodKey = "44b20652-2f1b-44cb-961d-c2e254d240a6";
+        public const string BatmanKey = "8c1f0e85-a665-4d2c-bfb3-0258c9b16b55";
+        public const string IvyKey = "5b41516a-f2cb-4256-88a7-3b91535639d1";
+        public const string HarleyKey = "1635bbb8-4168-4b42-a717-5b7813d913d7";
+        public const string CyborgKey = "0d615564-9ceb-4c19-aff2-95882c38ec5e";
 
-        private readonly HeroDbContext _context;
-        private readonly IUnitOfWorkManager _manager;
-        private readonly IDbContextFactory _factory;
+        private RepositoryFixture _fixture;
 
-        public RepositoryTests()
+        public RepositoryTests(RepositoryFixture fixture)
         {
-            var options = new DbContextOptionsBuilder();
-            options.UseInMemoryDatabase();
-
-            _manager = new UnitOfWorkManager();
-            _context = new HeroDbContext(options.Options);
-            _factory = new HeroDbContextFactory(_context);
-
-            SeedHeroes();
+            _fixture = fixture;
         }
 
         [Fact]
         public void CreateNewInstance_ShouldSuccess()
         {
-            var repository = new HeroRepository(_manager);
+            var repository = new HeroRepository(_fixture.UowManager);
             Assert.NotNull(repository);
         }
 
@@ -44,101 +31,27 @@ namespace Idea7.Repository.EntityFramework.Tests
         public void FindBatman_ShouldSuccess()
         {
             Hero batman;
-            using (new UnitOfWork.EntityFramework.UnitOfWork(_factory, _manager))
+            using (new UnitOfWork.EntityFramework.UnitOfWork(_fixture.DbContextFactory, _fixture.UowManager))
             {
-                var repository = new HeroRepository(_manager);
+                var repository = new HeroRepository(_fixture.UowManager);
                 batman = repository.Find(BatmanKey);
             }
 
             Assert.NotNull(batman);
+            Assert.Equal(batman.Name, "Batman");
         }
 
-        private void SeedHeroes()
+        [Fact]
+        public void FindCyborg_ShouldFoundNothing()
         {
-            var superman = new Hero
+            Hero cyborg;
+            using (new UnitOfWork.EntityFramework.UnitOfWork(_fixture.DbContextFactory, _fixture.UowManager))
             {
-                Id = SupermanKey,
-                Name = "Superman",
-                RealName = "Kal-El",
-                Origin = "Krypton"
-            };
+                var repository = new HeroRepository(_fixture.UowManager);
+                cyborg = repository.Find(CyborgKey);
+            }
 
-            var batman = new Hero
-            {
-                Id = BatmanKey,
-                Name = "Batman",
-                RealName = "Bruce Wayne",
-                Origin = "Gotham"
-            };
-
-            var zod = new Hero
-            {
-                Id = ZodKey,
-                Name = "General Zod",
-                RealName = "Dru-Zod",
-                Origin = "Krypton"
-            };
-
-            var ivy = new Hero
-            {
-                Id = IvyKey,
-                Name = "Poison Ivy",
-                RealName = "Pamela Lilian Isley",
-                Origin = "Seattle"
-            };
-
-            var harley = new Hero
-            {
-                Id = HarleyKey,
-                Name = "Harley Quinn",
-                RealName = "Harleen Frances Quinzel",
-                Origin = "Gotham"
-            };
-
-            var zodVsSuperman = new HeroRelationship
-            {
-                Id = Guid.NewGuid().ToString(),
-                Hero = superman,
-                ToHero = zod,
-                Relationship = Relationship.Enemy
-            };
-            var batmanVsIvy = new HeroRelationship
-            {
-                Id = Guid.NewGuid().ToString(),
-                Hero = batman,
-                ToHero = ivy,
-                Relationship = Relationship.Enemy
-            };
-            var batmanVsHarley = new HeroRelationship
-            {
-                Id = Guid.NewGuid().ToString(),
-                Hero = batman,
-                ToHero = harley,
-                Relationship = Relationship.Enemy
-            };
-            var ivyWithHarley = new HeroRelationship
-            {
-                Id = Guid.NewGuid().ToString(),
-                Hero = ivy,
-                ToHero = harley,
-                Relationship = Relationship.Friend
-            };
-
-            superman.Relationships.Add(zodVsSuperman);
-            zod.Relationships.Add(zodVsSuperman);
-
-            batman.Relationships.Add(batmanVsIvy);
-            batman.Relationships.Add(batmanVsHarley);
-            ivy.Relationships.Add(batmanVsIvy);
-            ivy.Relationships.Add(ivyWithHarley);
-            harley.Relationships.Add(batmanVsHarley);
-            harley.Relationships.Add(ivyWithHarley);
-
-            _context.Heroes.Add(superman);
-            _context.Heroes.Add(batman);
-            _context.Heroes.Add(zod);
-            _context.Heroes.Add(ivy);
-            _context.Heroes.Add(harley);
+            Assert.Null(cyborg);
         }
     }
 }
---
+++ Idea7.Repository/InMemoryRepository.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Idea7.Entity;
+using Idea7.Query;
+
+namespace Idea7.Repository
+{
+    /// <summary>
+    /// Initialize abstract in-memory repository
+    /// </summary>
+    public abstract class InMemoryRepository<TEntity, TKey> : IRepository<TEntity, TKey>
+        where TEntity : class, IEntity<TKey>
+    {
+        public abstract IList<TEntity> Data { get; }
+
+        public long Count(IQueryObject<TEntity> query)
+        {
+            return query.Count(Data.AsQueryable());
+        }
+
+        public IEnumerable<TEntity> Fetch(IQueryObject<TEntity> query)
+        {
+            return query.Fetch(Data.AsQueryable());
+        }
+
+        public TEntity FetchOne(IQueryObject<TEntity> query)
+        {
+            return query.FetchOne(Data.AsQueryable());
+        }
+
+        public TEntity Find(TKey id)
+        {
+            return Data.SingleOrDefault(s => s.Id.Equals(id));
+        }
+
+        public void Create(TEntity entity)
+        {
+            Data.Add(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            Delete(entity);
+            Create(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            Data.Remove(entity);
+        }
+    }
+}
